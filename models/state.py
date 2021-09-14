359f68552eb13ed0c6fad7d53f2892bec351e54f
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-14 18:48:38 +0000

    Fxiing errors

--- models/state.py
+++ models/state.py
@@ -17,8 +17,10 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="delete")
-
     else:
+        name = ""
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """
