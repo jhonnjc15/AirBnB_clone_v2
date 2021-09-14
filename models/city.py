afcea20d0a57192f063480ba52b001d04e02334c
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-14 19:22:38 +0000

    Adding more features

--- models/city.py
+++ models/city.py
@@ -2,10 +2,18 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This is the class for City
+    Attributes:
+    state_id: The state id
+    name: input name
+    """
+
     __tablename__ = 'cities'
+
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities')
