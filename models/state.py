7d88035cc0276549c396c942fd5f6de6344d01be
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-14 19:52:21 +0000

    Fixing the task 2

--- models/state.py
+++ models/state.py
@@ -11,16 +11,13 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="delete")
-    else:
-        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    else:
         @property
         def cities(self):
             """
